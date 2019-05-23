930e464083c974f436ed969693410b88b720f974
Claudio Andres Coronel <ccoronel@g-bapro.net>
2019-05-23 12:13:00 -0300

    Updates

---
+++ precache-manifest.b4e2720b5b35c8b50ef800412824c2b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a66dbcba66bfaf5fed1697199abbc5d",
+    "url": "/mercadolibros/static/media/la-comunidad-del-anillo-200x296.7a66dbcb.jpg"
+  },
+  {
+    "revision": "e56b841924ac729935e858cb59535fb7",
+    "url": "/mercadolibros/static/media/Login.e56b8419.png"
+  },
+  {
+    "revision": "c4ec58883dd24ad26188",
+    "url": "/mercadolibros/static/js/runtime~main.f8ff199c.js"
+  },
+  {
+    "revision": "59fea9209fb3b9ff507d",
+    "url": "/mercadolibros/static/js/main.b6d590cf.chunk.js"
+  },
+  {
+    "revision": "23e25e36c0a1a63a7ee6",
+    "url": "/mercadolibros/static/js/2.fc8ce8e3.chunk.js"
+  },
+  {
+    "revision": "59fea9209fb3b9ff507d",
+    "url": "/mercadolibros/static/css/main.4469de9e.chunk.css"
+  },
+  {
+    "revision": "1574ac9f4e8d7012bd9f58b2f142bdaa",
+    "url": "/mercadolibros/index.html"
+  }
+];
