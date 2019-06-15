f00dda5a634de7e2d78e052748644c49242734cf
Claudio Andres Coronel <ccoronel@g-bapro.net>
2019-06-14 21:27:53 -0300

    update 2

--- src/containers/BuscarUsuarios.js
+++ src/containers/BuscarUsuarios.js
@@ -3,6 +3,38 @@ import Navegacion from './Navegacion';
 import {FormGroup,Form,Button,InputGroup,FormControl,Glyphicon} from 'react-bootstrap';
 
 export default class BuscarUsuarios extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            Token: localStorage.getItem('sessionAdmin'),
+            username:""
+        }
+    }
+    async send() {
+        /* fetch("https://miapp-taller6.herokuapp.com/usuarios/usuarios").then((response)=>{
+            return response.json()})
+            .then(function(myJson) {
+                console.log(myJson);
+            }); */
+        var result = fetch("https://miapp-taller6.herokuapp.com/usuarios/", {
+        method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : 'bearer ' + localStorage.getItem('sessionAdmin'),
+          }
+        })
+        .then(console.log(result))
+        .then(response => {
+            
+            if (response.status === 200) {
+              return response.json();
+              
+            } else {
+              throw new Error('Something went wrong on api server!');
+            }
+          })
+        .catch(error => console.error('Error:', error))
+    }
     render() {
         return (
             <div>
@@ -20,7 +52,8 @@ export default class BuscarUsuarios extends Component {
                     </InputGroup>
 
                     <Button>Submit</Button>
-                    <Button>Cancel</Button>
+                    <Button
+                    onClick={this.send}>Cancel</Button>
                     </FormGroup>
                 </Form>
             </div>
