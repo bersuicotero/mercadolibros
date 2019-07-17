90c3576f51c721e5d0b5530cdeee78a2dbc9b396
Claudio Andres Coronel <ccoronel@g-bapro.net>
2019-07-17 15:12:23 -0300

    Updates

---
+++ precache-manifest.b639179b87be3a0503d89be1ffb95b4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a66dbcba66bfaf5fed1697199abbc5d",
+    "url": "/mercadolibros/static/media/la-comunidad-del-anillo-200x296.7a66dbcb.jpg"
+  },
+  {
+    "revision": "e56b841924ac729935e858cb59535fb7",
+    "url": "/mercadolibros/static/media/Login.e56b8419.png"
+  },
+  {
+    "revision": "c4ec58883dd24ad26188",
+    "url": "/mercadolibros/static/js/runtime~main.f8ff199c.js"
+  },
+  {
+    "revision": "dbf9bb4e913458ed8101",
+    "url": "/mercadolibros/static/js/main.9611069a.chunk.js"
+  },
+  {
+    "revision": "5f30e5b46b383f7d86c9",
+    "url": "/mercadolibros/static/js/2.531bd96b.chunk.js"
+  },
+  {
+    "revision": "dbf9bb4e913458ed8101",
+    "url": "/mercadolibros/static/css/main.4469de9e.chunk.css"
+  },
+  {
+    "revision": "dd54984fa709c63141558aeb44762d1c",
+    "url": "/mercadolibros/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9248ca9566ee0cc40845a8770257c5b.js"
+  "/mercadolibros/precache-manifest.b639179b87be3a0503d89be1ffb95b4f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/mercadolibros/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
